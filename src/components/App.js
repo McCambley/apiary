87458ddb53e6d95cd6c7fb27b9fcc8692f06b740
cjmaret <77926563+cjmaret@users.noreply.github.com>
2021-08-07 21:27:27 +0700

    added imports to App.js, cleaned App.css and index.css, add HashRouter

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="page">
+      
+    </div>
+  );
+}
+
+export default App;
