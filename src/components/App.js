361ee5db4431e2a6752711d6172635d427f3fe1b
cjmaret <77926563+cjmaret@users.noreply.github.com>
2021-08-08 15:11:26 +0700

    feat: add responsive header, install react-scroll

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import { Route } from 'react-router-dom';
+import Header from './header/Header.js';
 
 function App() {
   return (
     <div className="page">
-      
+      <Header />
     </div>
   );
 }
