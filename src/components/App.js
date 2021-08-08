9a6ac6c7fbf2ef70e24bce9ea63479989736af84
cjmaret <77926563+cjmaret@users.noreply.github.com>
2021-08-08 17:17:26 +0700

    rudimentary Lead JSX creation

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './App.css';
 import { Route } from 'react-router-dom';
 import Header from './header/Header.js';
+import Lead from './lead/Lead.js';
 
 function App() {
-  return <div className="page"><Header/></div>;
+  return <div className="page"><Header/><Lead /></div>;
 }
 
 export default App;
