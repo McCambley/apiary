5096c5bd4e543959cd0accfd1f019877c6ecceda
Denise Hung <denisehung87@gmail.com>
2021-08-08 12:51:05 +0200

    feat: add Hero section

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import { Route } from 'react-router-dom';
+import Hero from './Hero/Hero';
 
 function App() {
   return (
     <div className="page">
+    <Hero />
       
     </div>
   );
