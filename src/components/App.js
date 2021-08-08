8187b4d2274aed021b55008eb1a676437b2c3583
cjmaret <77926563+cjmaret@users.noreply.github.com>
2021-08-08 17:27:24 +0700

    create dummy page for header links

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,18 @@ import './App.css';
 import { Route } from 'react-router-dom';
 import Header from './header/Header.js';
 import Lead from './lead/Lead.js';
+import DummyPage from './dummy-page/DummyPage';
 
 function App() {
-  return <div className="page"><Header/><Lead /></div>;
+  return (
+    <div className="page">
+      <Header />
+      <Lead />
+      <Route path="/dummy-page">
+        <DummyPage />
+      </Route>
+    </div>
+  )
 }
 
 export default App;
--- src/components/lead/Lead.js
+++ src/components/lead/Lead.js
@@ -12,17 +12,17 @@ function Lead() {
                 <p className="lead__subtitle">By handing over assignments to students for work, you free experienced employees from routine tasks, as well as increase brand awareness.</p>
             </div>
             <div className="lead__image-card-group">
-                <div classNAme="image-card">
+                <div className="image-card">
                     <img className="image-card__image" src={ResourceSavingImage} alt="hands exchanging resources" />
                     <p className="image-card__title">Resource saving</p>
                     <p className="image-card__description">Delegate tasks for free and get results that can be used in your future work.</p>
                 </div>
-                <div classNAme="image-card">
+                <div className="image-card">
                     <img className="image-card__image" src={PublicizeImage} alt="megaphone and media marketing" />
                     <p className="image-card__title">Publicize</p>
                     <p className="image-card__description">We take pride in the real-world value of the tasks that our students perform and we share them in all available channels.</p>
                 </div>
-                <div classNAme="image-card">
+                <div className="image-card">
                     <img className="image-card__image" src={PRBoostImage} alt="rocket ship" />
                     <p className="image-card__title">PR boost</p>
                     <p className="image-card__description">This will increase the visibility of the company among job seekers and attract those who share the values of your company.</p>
