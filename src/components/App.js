1edac9a82c297da59ca618a11e7246c6bcc90257
cjmaret <77926563+cjmaret@users.noreply.github.com>
2021-08-08 18:52:58 +0700

    fix: hero image path, folder capitalized

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Route } from 'react-router-dom';
-import Hero from './hero/Hero';
+import Hero from './hero/Hero.js';
 import Header from './header/Header.js';
 import Lead from './lead/Lead.js';
 import DummyPage from './dummy-page/DummyPage';
