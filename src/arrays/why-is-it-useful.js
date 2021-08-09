c8ed5201721a5132f97685690d5476f9a6deab16
Rebecca Burch <58955802+thinklikeadesigner@users.noreply.github.com>
2021-08-09 12:26:08 +0300

    refactored lead and chooseus to fit array pattern

--- src/arrays/why-is-it-useful.js
+++ src/arrays/why-is-it-useful.js
@@ -1,23 +1,28 @@
-[
+
+import ResourceSavingImage from '../images/hands.png';
+import PublicizeImage from '../images/megaphone.png';
+import PRBoostImage from '../images/rocket.png';
+
+export const useful = [
   {
     "id": 1,
     "title": "Resource saving",
     "subTitle": "Delegate tasks for free and get results that can be used in your future work.",
-    "image": "",
-    "alt": ""
+    "image": ResourceSavingImage,
+    "alt": "hands exchanging resources"
   },
   {
     "id": 2,
     "title": "Publicize",
     "subTitle": "We take pride in the real-world value of the tasks that our students perform and we share them in all available channels.",
-    "image": "",
-    "alt": ""
+    "image": PublicizeImage,
+    "alt": "megaphone and media marketing"
   },
   {
     "id": 3,
     "title": "PR boost",
     "subTitle": "This will increase the visibility of the company among job seekers and attract those who share the values of your company.",
-    "image": "",
-    "alt": ""
+    "image": PRBoostImage,
+    "alt": "rocket ship"
   }
 ]
--- src/components/selling-points/SellingPoints.js
+++ src/components/selling-points/SellingPoints.js
@@ -8,25 +8,18 @@ function SellingPoints(props) {
                 <h2 className="selling-points__title">{props.title}</h2>
                 <p className="selling-points__subtitle">{props.subtitle}</p>
             </div>
-            <div className="selling-points__image-card-group">
-                <div className="selling-points__image-card-wrap-container">
-                    <div className="image-card">
-                        <img className='image-card__image' src={props.cardAImage} alt={props.cardAAlt} />
-                        <p className="image-card__title">{props.cardATitle}</p>
-                        <p className="image-card__description">{props.cardADescription}</p>
-                    </div>
-                </div>
-                <div className="image-card">
-                    <img className='image-card__image' src={props.cardBImage} alt={props.cardBAlt} />
-                    <p className="image-card__title">{props.cardBTitle}</p>
-                    <p className="image-card__description">{props.cardBDescription}</p>
-                </div>
-                <div className="image-card">
-                    <img className='image-card__image' src={props.cardCImage} alt={props.cardCAlt} />
-                    <p className="image-card__title">{props.cardCTitle}</p>
-                    <p className="image-card__description">{props.cardCDescription}</p>
-                </div>
-            </div>
+            <ul className="selling-points__image-card-group" style={{listStyle: "none"}} >
+               {props.data.map((item) => 
+             (  <li key={item.id} className="selling-points__image-card-wrap-container">
+               <div className="image-card">
+                   <img className='image-card__image' src={item.image} alt={item.alt} />
+                   <p className="image-card__title">{item.title}</p>
+                   <p className="image-card__description">{item.subTitle}</p>
+               </div>
+           </li>)
+           )}
+               
+            </ul>
         </section>
     )
 }
