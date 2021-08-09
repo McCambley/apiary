25f2c1cfac2d5a63a0e2ac295dd59c9191553e1c
Rebecca Burch <58955802+thinklikeadesigner@users.noreply.github.com>
2021-08-09 13:10:08 +0300

    finished the arrays

---
+++ src/arrays/what-requests.js
@@ -0,0 +1,27 @@
+import DataAnalysis from '../images/data-analysis.png';
+import WebDev from '../images/web-dev.png';
+import DataScience from '../images/data-science.png';
+
+export const requests = [
+  {
+    "id": 1,
+    "hashtag": "#programing",
+    "title": "Web development",
+    "image": DataAnalysis,
+    "alt": "website drawing"
+  },
+  {
+    "id": 2,
+    "hashtag": "#analysis",
+    "title": "Data analysis",
+    "image": WebDev,
+    "alt": "snowglobe with math inside"
+  },
+  {
+    "id": 3,
+    "hashtag": "#analysis",
+    "title": "Data science",
+    "image": DataScience,
+    "alt": "rubik's cube"
+  }
+]
