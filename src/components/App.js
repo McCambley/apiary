4c6dbd8b8b777ae2b733e057a7498213e3973124
Denise Hung <denisehung87@gmail.com>
2021-08-10 00:20:53 +0200

    feat: create Requests component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import { Route } from 'react-router-dom';
-import Hero from './hero/Hero.js';
-import Header from './header/Header.js';
-import Lead from './lead/Lead.js';
-import DummyPage from './dummy-page/DummyPage.js';
-import ChooseUs from './choose-us/ChooseUs.js';
+import Hero from './hero/Hero';
+import Header from './header/Header';
+import Lead from './lead/Lead';
+import Requests from './requests/Requests';
+import DummyPage from './dummy-page/DummyPage';
+import ChooseUs from './choose-us/ChooseUs';
 import Footer from './footer/Footer';
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
       <Route path="/dummy-page">
         <DummyPage />
       </Route>
+      <Requests />
       <ChooseUs />
       <Footer />
     </div>
