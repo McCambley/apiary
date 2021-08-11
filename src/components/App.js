ddd966b457bb28a126e6ca7a16377f5dd47e226e
Rebecca Burch <58955802+thinklikeadesigner@users.noreply.github.com>
2021-08-12 00:38:41 +0200

    contentful basic api set up

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Route } from 'react-router-dom';
 import Hero from './hero/Hero';
+import client from './client';
 import Header from './header/Header';
 import Lead from './lead/Lead';
 import Requests from './requests/Requests';
@@ -10,6 +11,17 @@ import ChooseUs from './choose-us/ChooseUs';
 import Footer from './footer/Footer';
 
 function App() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    client
+      .getEntries()
+      .then((response) => {
+        console.log(response);
+      })
+      .catch(console.error);
+  });
+
   return (
     <div className="page">
       <Header />
