9d3a36d10de3b8ab3425859034f5f1a7c418008e
cjmaret <77926563+cjmaret@users.noreply.github.com>
2021-08-12 17:42:17 +0700

    refactor: remove another unnecessary array variable in ProfessionHero

--- src/components/profession-hero/ProfessionHero.js
+++ src/components/profession-hero/ProfessionHero.js
@@ -10,7 +10,6 @@ import professionHeroCards from '../../arrays/profession-hero-cards';
 
 function ProfessionHero() {
   const { id } = useParams();
-  // const professionTitles = professionHeroTitles;
   const professionTitle = professionHeroTitles.find((page) => page.id === id);
   const professionCard = professionHeroCards.find((cardGroup) => cardGroup.id === id);
 
