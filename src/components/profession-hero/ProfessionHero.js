dea62793233cafc625e9dc215877e1f11d7e16a4
cjmaret <77926563+cjmaret@users.noreply.github.com>
2021-08-12 13:30:00 +0700

    fix: Prettier for ProfessionHero

--- src/components/profession-hero/ProfessionHero.js
+++ src/components/profession-hero/ProfessionHero.js
@@ -1,8 +1,8 @@
-import React from "react";
-import "./ProfessionHero.css";
-import WebDev from "../../images/web-dev.png";
-import DataAnalysis from "../../images/data-analysis.png";
-import DataScience from "../../images/data-science.png";
+import React from 'react';
+import './ProfessionHero.css';
+import WebDev from '../../images/web-dev.png';
+import DataAnalysis from '../../images/data-analysis.png';
+import DataScience from '../../images/data-science.png';
 
 function ProfessionHero({ props }) {
   return (
@@ -14,10 +14,9 @@ function ProfessionHero({ props }) {
             {props.title}What web development requests can we help with?
           </h2>
           <p className="profession__subtitle">
-            {props.description}Practicum by Yandex students are ready to build
-            your website from your design or if necessary, we can ask our
-            colleagues from the design team to create a unique design for your
-            company and a website based on this design.
+            {props.description}Practicum by Yandex students are ready to build your website from
+            your design or if necessary, we can ask our colleagues from the design team to create a
+            unique design for your company and a website based on this design.
           </p>
         </div>
       </div>
@@ -29,8 +28,8 @@ function ProfessionHero({ props }) {
               {props.cardATitle}Create a one page website from scratch
             </p>
             <p className="profession-card__subtitle">
-              {props.cardASubtitle}We can create a landing page for a new
-              project or refresh an existing one
+              {props.cardASubtitle}We can create a landing page for a new project or refresh an
+              existing one
             </p>
           </div>
         </div>
@@ -41,8 +40,8 @@ function ProfessionHero({ props }) {
               {props.cardBTitle}We make up your website design
             </p>
             <p className="profession-card__subtitle">
-              {props.cardBSubtitle}If you already have a website design, we will
-              gladly prepare an adaptive layout.
+              {props.cardBSubtitle}If you already have a website design, we will gladly prepare an
+              adaptive layout.
             </p>
           </div>
         </div>
@@ -50,12 +49,11 @@ function ProfessionHero({ props }) {
           <div className="profession-card__content-group">
             <h3 className="profession-card__number">3</h3>
             <p className="profession-card__title">
-              {props.cardCTitle}Let's create a complex multi-page website for
-              the project
+              {props.cardCTitle}Let's create a complex multi-page website for the project
             </p>
             <p className="profession-card__subtitle">
-              {props.cardCSubtitle}We are able to work on highly complex
-              websites with deeply nested pages.
+              {props.cardCSubtitle}We are able to work on highly complex websites with deeply nested
+              pages.
             </p>
           </div>
         </div>
