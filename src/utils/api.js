5e431f4c932cfecb22ab24c994aee27986303ff3
McCambley <jakemccamb16@gmail.com>
2021-08-14 22:52:52 -0400

    Dev: create env and api

---
+++ src/utils/api.js
@@ -0,0 +1,11 @@
+// import { ApolloClient, InMemoryCache } from '@apollo/client';
+
+// const { REACT_APP_ACCESS_TOKEN, REACT_APP_SPACE_ID } = process.env;
+
+// const client = new ApolloClient({
+//   cache: new InMemoryCache(),
+//   uri: `https://graphql.contentful.com/content/v1/spaces/${REACT_APP_SPACE_ID}`,
+//   headers: {
+//     Authorization: `Bearer ${REACT_APP_ACCESS_TOKEN}`,
+//   },
+// });
