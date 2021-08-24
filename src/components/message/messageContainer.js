ba58bfe3e6d19a774cdceef3806df60175cf6a2e
Rebecca Burch <58955802+thinklikeadesigner@users.noreply.github.com>
2021-08-23 21:26:14 -0500

    uselayouteffect deleted

--- src/components/message/messageContainer.js
+++ src/components/message/messageContainer.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/button-has-type */
-import React, { useEffect, useLayoutEffect } from 'react';
+import React, { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { Inner, Container, Item, Bubble, Title, Reply, Image } from './styledMessages';
